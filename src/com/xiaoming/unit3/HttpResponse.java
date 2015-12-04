7922f6d0a827de2339c9af26489b3e40a894e188
termonitor <pxm2525@163.com>
2015-12-04 21:51:59 +0800

    unit3 HttpResponse

--- src/com/xiaoming/unit3/HttpResponse.java
+++ src/com/xiaoming/unit3/HttpResponse.java
@@ -1,21 +1,38 @@
 package com.xiaoming.unit3;
 
+import com.xiaoming.unit2.Constants;
+import org.apache.catalina.Response;
+import org.apache.catalina.connector.ResponseStream;
+import org.apache.catalina.util.CookieTools;
+
 import javax.servlet.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.Enumeration;
-import java.util.Locale;
-import java.util.Map;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by panxiaoming on 15/12/4.
  */
-public class HttpResponse implements ServletRequest {
+public class HttpResponse implements HttpServletResponse {
 
+    private static final int BUFFER_SIZE = 1024;
     private HttpRequest request = null;
     private OutputStream output = null;
+    private PrintWriter writer = null;
+    protected byte[] buffer = new byte[BUFFER_SIZE];
+    protected int bufferCount = 0;
+    protected boolean committed = false;
+    protected int contentCount = 0;
+    protected int contentLength = -1;
+    protected String contentType = null;
+    protected String encoding = null;
+    protected ArrayList cookies = new ArrayList();
+    protected HashMap headers = new HashMap();
+    protected final SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy HH:mm:ss zzz", Locale.CHINESE);
+    protected String message = getStatusMessage(HttpServletResponse.SC_OK);
+    protected int status = HttpServletResponse.SC_OK;
 
     public HttpResponse(OutputStream output) {
         this.output = output;
@@ -25,183 +42,483 @@ public class HttpResponse implements ServletRequest {
         this.request = request;
     }
 
+    public void finishResponse() {
+        if(writer != null) {
+            writer.flush();
+            writer.close();
+        }
+    }
+
+    public int getContentLength() {
+        return contentLength;
+    }
+
+    public String getProtocol() {
+        return request.getProtocol();
+    }
+
+    protected String getStatusMessage(int status) {
+        switch (status) {
+            case SC_OK:
+                return ("OK");
+            case SC_ACCEPTED:
+                return ("Accepted");
+            case SC_BAD_GATEWAY:
+                return ("Bad Gateway");
+            case SC_BAD_REQUEST:
+                return ("Bad Request");
+            case SC_CONFLICT:
+                return ("Conflict");
+            case SC_CONTINUE:
+                return ("Continue");
+            case SC_CREATED:
+                return ("Created");
+            case SC_EXPECTATION_FAILED:
+                return ("Expectation Failed");
+            case SC_FORBIDDEN:
+                return ("Forbidden");
+            case SC_GATEWAY_TIMEOUT:
+                return ("Gateway Timeout");
+            case SC_GONE:
+                return ("Gone");
+            case SC_HTTP_VERSION_NOT_SUPPORTED:
+                return ("HTTP Version Not Supported");
+            case SC_INTERNAL_SERVER_ERROR:
+                return ("Internal Server Error");
+            case SC_LENGTH_REQUIRED:
+                return ("Length Required");
+            case SC_METHOD_NOT_ALLOWED:
+                return ("Method Not Allowed");
+            case SC_MOVED_PERMANENTLY:
+                return ("Moved Permanently");
+            case SC_MOVED_TEMPORARILY:
+                return ("Moved Temporarily");
+            case SC_MULTIPLE_CHOICES:
+                return ("Multiple Choices");
+            case SC_NO_CONTENT:
+                return ("No Content");
+            case SC_NON_AUTHORITATIVE_INFORMATION:
+                return ("Non-Authoritative Information");
+            case SC_NOT_ACCEPTABLE:
+                return ("Not Acceptable");
+            case SC_NOT_FOUND:
+                return ("Not Found");
+            case SC_NOT_IMPLEMENTED:
+                return ("Not Implemented");
+            case SC_NOT_MODIFIED:
+                return ("Not Modified");
+            case SC_PARTIAL_CONTENT:
+                return ("Partial Content");
+            case SC_PAYMENT_REQUIRED:
+                return ("Payment Required");
+            case SC_PRECONDITION_FAILED:
+                return ("Precondition Failed");
+            case SC_PROXY_AUTHENTICATION_REQUIRED:
+                return ("Proxy Authentication Required");
+            case SC_REQUEST_ENTITY_TOO_LARGE:
+                return ("Request Entity Too Large");
+            case SC_REQUEST_TIMEOUT:
+                return ("Request Timeout");
+            case SC_REQUEST_URI_TOO_LONG:
+                return ("Request URI Too Long");
+            case SC_REQUESTED_RANGE_NOT_SATISFIABLE:
+                return ("Requested Range Not Satisfiable");
+            case SC_RESET_CONTENT:
+                return ("Reset Content");
+            case SC_SEE_OTHER:
+                return ("See Other");
+            case SC_SERVICE_UNAVAILABLE:
+                return ("Service Unavailable");
+            case SC_SWITCHING_PROTOCOLS:
+                return ("Switching Protocols");
+            case SC_UNAUTHORIZED:
+                return ("Unauthorized");
+            case SC_UNSUPPORTED_MEDIA_TYPE:
+                return ("Unsupported Media Type");
+            case SC_USE_PROXY:
+                return ("Use Proxy");
+            case 207:       // WebDAV
+                return ("Multi-Status");
+            case 422:       // WebDAV
+                return ("Unprocessable Entity");
+            case 423:       // WebDAV
+                return ("Locked");
+            case 507:       // WebDAV
+                return ("Insufficient Storage");
+            default:
+                return ("HTTP Response Status " + status);
+        }
+    }
+
+    public OutputStream getStream() {
+        return this.output;
+    }
+
+    protected void sendHeaders() throws IOException {
+        if(isCommitted()) {
+            return;
+        }
+        OutputStreamWriter osr = null;
+        try {
+            osr = new OutputStreamWriter(getStream(), getCharacterEncoding());
+        } catch(UnsupportedEncodingException e) {
+//            e.printStackTrace();
+            osr = new OutputStreamWriter(getStream());
+        }
+        final PrintWriter outputWriter = new PrintWriter(osr);
+        outputWriter.print(this.getProtocol());
+        outputWriter.print(" ");
+        outputWriter.print(status);
+        if(message != null) {
+            outputWriter.print(" ");
+            outputWriter.print(message);
+        }
+        outputWriter.print("\r\n");
+        if(getContentType() != null) {
+            outputWriter.print("Content-Type: " + getContentType() + "\r\n");
+        }
+        if(getContentLength() >= 0) {
+            outputWriter.print("Content-Length: " + getContentLength() + "\r\n");
+        }
+        synchronized (headers) {
+            Iterator names = headers.entrySet().iterator();
+            while(names.hasNext()) {
+                String name = (String) names.next();
+                ArrayList values = (ArrayList) headers.get(name);
+                Iterator items = values.iterator();
+                while(items.hasNext()) {
+                    String value = (String) items.next();
+                    outputWriter.print(name);
+                    outputWriter.print(": ");
+                    outputWriter.print(value);
+                    outputWriter.print("\r\n");
+                }
+            }
+        }
+        synchronized (cookies) {
+            Iterator items = cookies.iterator();
+            while(items.hasNext()) {
+                Cookie cookie = (Cookie) items.next();
+                outputWriter.print(CookieTools.getCookieHeaderName(cookie));
+                outputWriter.print(": ");
+                outputWriter.print(CookieTools.getCookieHeaderValue(cookie));
+                outputWriter.print("\r\n");
+            }
+        }
+        outputWriter.print("\r\n");
+        outputWriter.flush();
+
+        committed = true;
+    }
+
+    public void sendStaticResource() throws IOException {
+        byte[] bytes = new byte[BUFFER_SIZE];
+        FileInputStream fis = null;
+        try {
+            File file = new File(Constants.WEB_ROOT, request.getUri());
+            if(file.exists()) {
+                fis = new FileInputStream(file);
+                int ch = fis.read(bytes, 0, BUFFER_SIZE);
+                while(ch != -1) {
+                    output.write(bytes, 0, ch);
+                    ch = fis.read(bytes, 0, BUFFER_SIZE);
+                }
+            } else {
+                String errorMessage = "HTTP/1.1 404 File Not Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: 23\r\n" +
+                        "\r\n" +
+                        "<h1>File Not Found</h1>";
+                output.write(errorMessage.getBytes());
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(fis != null)
+                fis.close();
+        }
+    }
+
+    public void write(int b) throws IOException {
+        if(bufferCount >= buffer.length) {
+            flushBuffer();
+        }
+        buffer[bufferCount++] = (byte)b;
+        contentCount++;
+    }
+
+    public void write(byte b[]) throws IOException {
+        write(b, 0, b.length);
+    }
+
+    public void write(byte b[], int off, int len) throws IOException {
+        if(len == 0)
+            return;
+        if(len <= (buffer.length - bufferCount)) {
+            System.arraycopy(b, off, buffer, bufferCount, len);
+            bufferCount += len;
+            contentCount += len;
+            return;
+        }
+        flushBuffer();
+        int iterations = len / buffer.length;
+        int leftoverStart = iterations * buffer.length;
+        int leftoverLen = len - leftoverStart;
+        for (int i = 0; i < iterations; i++) {
+            write(b, off + (i * buffer.length), buffer.length);
+        }
+        if(leftoverLen > 0)
+            write(b, off + leftoverStart, leftoverLen);
+    }
+
     @Override
-    public Object getAttribute(String s) {
+    public void addCookie(Cookie cookie) {
+        if(isCommitted())
+            return;
+        synchronized(cookies) {
+            cookies.add(cookie);
+        }
+    }
+
+    @Override
+    public boolean containsHeader(String name) {
+        synchronized (headers) {
+            return (headers.get(name)!=null);
+        }
+    }
+
+    @Override
+    public String encodeURL(String url) {
         return null;
     }
 
     @Override
-    public Enumeration<String> getAttributeNames() {
+    public String encodeRedirectURL(String url) {
+        return null;
+    }
+
+    @Override
+    public String encodeUrl(String url) {
+        return encodeURL(url);
+    }
+
+    @Override
+    public String encodeRedirectUrl(String url) {
+        return encodeRedirectURL(url);
+    }
+
+    @Override
+    public void sendError(int i, String s) throws IOException {
+
+    }
+
+    @Override
+    public void sendError(int i) throws IOException {
+
+    }
+
+    @Override
+    public void sendRedirect(String s) throws IOException {
+
+    }
+
+    @Override
+    public void setDateHeader(String name, long value) {
+        if(isCommitted())
+            return;
+        setHeader(name, format.format(new Date(value)));
+    }
+
+    @Override
+    public void addDateHeader(String name, long value) {
+        if(isCommitted())
+            return;
+        addHeader(name, format.format(new Date(value)));
+    }
+
+    @Override
+    public void setHeader(String name, String value) {
+        if(isCommitted())
+            return;
+        ArrayList values = new ArrayList();
+        values.add(value);
+        synchronized (headers) {
+            headers.put(name, values);
+        }
+        String match = name.toLowerCase();
+        if (match.equals("content-length")) {
+            int contentLength = -1;
+            try {
+                contentLength = Integer.parseInt(value);
+            }
+            catch (NumberFormatException e) {
+                ;
+            }
+            if (contentLength >= 0)
+                setContentLength(contentLength);
+        }
+        else if (match.equals("content-type")) {
+            setContentType(value);
+        }
+    }
+
+    @Override
+    public void addHeader(String name, String value) {
+        if(isCommitted())
+            return;
+        synchronized (headers) {
+            ArrayList values = (ArrayList) headers.values();
+            if(values == null) {
+                values = new ArrayList();
+                headers.put(name, values);
+            }
+
+            values.add(value);
+        }
+    }
+
+    @Override
+    public void setIntHeader(String name, int value) {
+        if (isCommitted())
+            return;
+        setHeader(name, "" + value);
+    }
+
+    @Override
+    public void addIntHeader(String name, int value) {
+        if(isCommitted())
+            return;
+        addHeader(name, "" + value);
+    }
+
+    @Override
+    public void setStatus(int i) {
+
+    }
+
+    @Override
+    public void setStatus(int i, String s) {
+
+    }
+
+    @Override
+    public int getStatus() {
+        return 0;
+    }
+
+    @Override
+    public String getHeader(String s) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaders(String s) {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getHeaderNames() {
         return null;
     }
 
     @Override
     public String getCharacterEncoding() {
-        return null;
-    }
-
-    @Override
-    public void setCharacterEncoding(String s) throws UnsupportedEncodingException {
-
-    }
-
-    @Override
-    public int getContentLength() {
-        return 0;
+        if(encoding == null)
+            return "UTF-8";
+        else
+            return encoding;
     }
 
     @Override
     public String getContentType() {
+        return contentType;
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
         return null;
     }
 
     @Override
-    public ServletInputStream getInputStream() throws IOException {
-        return null;
+    public PrintWriter getWriter() throws IOException {
+//        ResponseStream responseStream = new ResponseStream(this);
+//        responseStream.setCommit(false);
+//        OutputStreamWriter osr = new OutputStreamWriter(responseStream, getCharacterEncoding());
+//        writer = new PrintWriter(osr);
+        return writer;
     }
 
     @Override
-    public String getParameter(String s) {
-        return null;
+    public void setCharacterEncoding(String s) {
+
     }
 
     @Override
-    public Enumeration<String> getParameterNames() {
-        return null;
+    public void setContentLength(int length) {
+        if(isCommitted())
+            return;
+        this.contentLength = length;
     }
 
     @Override
-    public String[] getParameterValues(String s) {
-        return new String[0];
+    public void setContentType(String s) {
+
     }
 
     @Override
-    public Map<String, String[]> getParameterMap() {
-        return null;
+    public void setBufferSize(int i) {
+
     }
 
     @Override
-    public String getProtocol() {
-        return null;
-    }
-
-    @Override
-    public String getScheme() {
-        return null;
-    }
-
-    @Override
-    public String getServerName() {
-        return null;
-    }
-
-    @Override
-    public int getServerPort() {
+    public int getBufferSize() {
         return 0;
     }
 
     @Override
-    public BufferedReader getReader() throws IOException {
-        return null;
+    public void flushBuffer() throws IOException {
+        if(bufferCount >= 0) {
+            try {
+                output.write(buffer, 0, bufferCount);
+            } finally {
+                bufferCount = 0;
+            }
+        }
     }
 
     @Override
-    public String getRemoteAddr() {
-        return null;
-    }
-
-    @Override
-    public String getRemoteHost() {
-        return null;
-    }
-
-    @Override
-    public void setAttribute(String s, Object o) {
+    public void resetBuffer() {
 
     }
 
     @Override
-    public void removeAttribute(String s) {
+    public boolean isCommitted() {
+        return committed;
+    }
 
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setLocale(Locale locale) {
+        if (isCommitted())
+            return;
+        String language = locale.getLanguage();
+        if ((language != null) && (language.length() > 0)) {
+            String country = locale.getCountry();
+            StringBuffer value = new StringBuffer(language);
+            if ((country != null) && (country.length() > 0)) {
+                value.append('-');
+                value.append(country);
+            }
+            setHeader("Content-Language", value.toString());
+        }
     }
 
     @Override
     public Locale getLocale() {
-        return null;
-    }
-
-    @Override
-    public Enumeration<Locale> getLocales() {
-        return null;
-    }
-
-    @Override
-    public boolean isSecure() {
-        return false;
-    }
-
-    @Override
-    public RequestDispatcher getRequestDispatcher(String s) {
-        return null;
-    }
-
-    @Override
-    public String getRealPath(String s) {
-        return null;
-    }
-
-    @Override
-    public int getRemotePort() {
-        return 0;
-    }
-
-    @Override
-    public String getLocalName() {
-        return null;
-    }
-
-    @Override
-    public String getLocalAddr() {
-        return null;
-    }
-
-    @Override
-    public int getLocalPort() {
-        return 0;
-    }
-
-    @Override
-    public ServletContext getServletContext() {
-        return null;
-    }
-
-    @Override
-    public AsyncContext startAsync() throws IllegalStateException {
-        return null;
-    }
-
-    @Override
-    public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
-        return null;
-    }
-
-    @Override
-    public boolean isAsyncStarted() {
-        return false;
-    }
-
-    @Override
-    public boolean isAsyncSupported() {
-        return false;
-    }
-
-    @Override
-    public AsyncContext getAsyncContext() {
-        return null;
-    }
-
-    @Override
-    public DispatcherType getDispatcherType() {
         return null;
     }
 }

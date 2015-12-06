7adb7efe6a1528f0bf815957425ed28dfcc0ce40
termonitor <pxm2525@163.com>
2015-12-06 18:46:58 +0800

    unit3 RequestStream

--- src/com/xiaoming/unit3/RequestStream.java
+++ src/com/xiaoming/unit3/RequestStream.java
@@ -27,6 +27,44 @@ public class RequestStream extends ServletInputStream {
 
     @Override
     public int read() throws IOException {
-        return 0;
+        if(closed) {
+            throw new IOException(sm.getString("requestStream.read.closed"));
+        }
+        if((length > 0) && (count >= length))
+            return -1;
+        int b = stream.read();
+        if(b >= 0)
+            count++;
+        return b;
+    }
+
+    public void close() throws IOException {
+        if(closed) {
+            throw new IOException(sm.getString("requestStream.close.closed"));
+        }
+        if(length > 0) {
+            while(count < length) {
+                int b = read();
+                if(b < 0)
+                    break;
+            }
+        }
+        closed = true;
+    }
+
+    public int read(byte b[]) throws IOException {
+        return read(b, 0, b.length);
+    }
+
+    public int read(byte b[], int off, int len) throws IOException {
+        int toRead = len;
+        if(length > 0) {
+            if(count >= length)
+                return -1;
+            if((count + len) > length)
+                toRead = length - count;
+        }
+        int actuallyRead = super.read(b, off, toRead);
+        return actuallyRead;
     }
 }

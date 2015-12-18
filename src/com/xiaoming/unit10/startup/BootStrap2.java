91669f9b979f6605b7f88fadae2fce353e254e26
termonitor <pxm2525@163.com>
2015-12-18 20:52:29 +0800

    unit10 done. but not use

---
+++ src/com/xiaoming/unit10/startup/BootStrap2.java
@@ -0,0 +1,69 @@
+package com.xiaoming.unit10.startup;
+
+import com.xiaoming.unit10.core.SimpleContextConfig;
+import com.xiaoming.unit10.core.SimpleWrapper;
+import com.xiaoming.unit10.realm.SimpleUserDatabaseRealm;
+import org.apache.catalina.*;
+import org.apache.catalina.connector.http.HttpConnector;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.deploy.LoginConfig;
+import org.apache.catalina.deploy.SecurityCollection;
+import org.apache.catalina.deploy.SecurityConstraint;
+import org.apache.catalina.loader.WebappLoader;
+
+/**
+ * Created by panxiaoming on 15/12/18.
+ */
+public class BootStrap2 {
+    public static void main(String[] args) {
+        System.setProperty("catalina.base", System.getProperty("user.dir"));
+        Connector connector = new HttpConnector();
+        Wrapper wrapper1 = new SimpleWrapper();
+        wrapper1.setName("Primitive");
+        wrapper1.setServletClass("PrimitiveServlet");
+        Wrapper wrapper2 = new SimpleWrapper();
+        wrapper2.setName("Modern");
+        wrapper2.setServletClass("ModernServlet");
+
+        Context context = new StandardContext();
+        context.setPath("/myApp");
+        context.setDocBase("myApp");
+        LifecycleListener listener = new SimpleContextConfig();
+        ((Lifecycle) context).addLifecycleListener(listener);
+        context.addChild(wrapper1);
+        context.addChild(wrapper2);
+
+        Loader loader = new WebappLoader();
+        context.setLoader(loader);
+        context.addServletMapping("/Primitive", "Primitive");
+        context.addServletMapping("/Modern", "Modern");
+        SecurityCollection securityCollection = new SecurityCollection();
+        securityCollection.addPattern("/");
+        securityCollection.addMethod("GET");
+
+        SecurityConstraint constraint = new SecurityConstraint();
+        constraint.addCollection(securityCollection);
+        constraint.addAuthRole("manager");
+        LoginConfig loginConfig = new LoginConfig();
+        loginConfig.setRealmName("Simple User Database Realm");
+
+        Realm realm = new SimpleUserDatabaseRealm();
+        ((SimpleUserDatabaseRealm) realm).createDatabase("conf/tomcat-users.xml");
+        context.setRealm(realm);
+        context.addConstraint(constraint);
+        context.setLoginConfig(loginConfig);
+
+        connector.setContainer(context);
+
+        try {
+            connector.initialize();
+            ((Lifecycle) connector).start();
+            ((Lifecycle) context).start();
+
+            System.in.read();
+            ((Lifecycle) context).stop();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
